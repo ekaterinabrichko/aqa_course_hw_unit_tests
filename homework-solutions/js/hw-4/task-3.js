0dc6c3320292a022679788b0e6043bd3e9457716
ekaterinabrichko <ekaterina.brichko@gmail.com>
2025-08-02 23:26:17 +0200

    HW 4

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -2,7 +2,23 @@
 
 (НЕ ОБЯЗАТЕЛЬНО)
 
-Преобразовать Task 2 таким образом, чтобы значение НАПРИМЕР '2' (т.е. ЛЮБАЯ строка в которой лежат ТОЛЬКО ЦИФРЫ) пропускалось, 
-  преобразовываясь в number
+Преобразовать Task 2 таким образом, чтобы значение НАПРИМЕР '2' 
+(т.е. ЛЮБАЯ строка в которой лежат ТОЛЬКО ЦИФРЫ) пропускалось, преобразовываясь в number
 
 */
+  const minAge = 18;
+  const maxAge = 60;
+  let age = "zoom";
+  age = +age;
+
+  if (typeof age !== 'number') {
+    console.log('Incorrect data type');
+  } else if (age < minAge) {
+    console.log("You don't have access cause your age is " + age + " It's less than " + minAge);
+  } else if (age >= minAge && age < maxAge) {
+    console.log("Welcome!");
+  } else if (age > maxAge){
+    console.log("Keep calm and look Culture channel");
+  } else {
+    console.log("Technical work");
+  }
